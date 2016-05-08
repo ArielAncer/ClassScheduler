5f240d0c53a15f47d252c9c0d235dc9701aab6e3
Aaron Landy <aaron@thecompassmag.com>
2016-05-08 08:00:55 -0400

    override working

--- ClassScheduler/client/calendar.js
+++ ClassScheduler/client/calendar.js
@@ -1,10 +1,19 @@
 Template.calendar.rendered = function () {
-    var fc = $('#calendar');
+    // setInterval(function() {
+    //   $('#calendar').fullCalendar('removeEvents')
+    //   $('#calendar').fullCalendar('refetchEvents');
+    //   console.log(CalEvents.find({}).fetch());
+    // }, 1000)
+    $('#mycalendar').fullCalendar('removeEvents')
+    $('#mycalendar').fullCalendar('refetchEvents');
+    console.log(CalEvents.find({}).fetch())
+    //$('#mycalendar').fullCalendar( 'renderEvent', {title: "Accounting Principles II", start: "2016-05-10 09:00", end: "2016-05-10 10:15"});
+    //
     this.autorun(function () {
         //1) trigger event re-rendering when the collection is changed in any way
         //2) find all, because we've already subscribed to a specific range
-        CalEvents.find();
-        fc.fullCalendar('refetchEvents');
+        $('#mycalendar').fullCalendar('removeEvents')
+        $('#mycalendar').fullCalendar('refetchEvents');
     });
 };
 
@@ -17,5 +26,16 @@ Template.calendar.helpers({
       id: "calendar",
       events: CalEvents.find({}).fetch()
     };
+  },
+    added: function(id) {
+    $('#mycalendar').fullCalendar('refetchEvents');
+  },
+
+  removed: function(id) {
+    $('#mycalendar').fullCalendar('refetchEvents');
+  },
+
+  changed: function(old, newd) {
+    $('#mycalendar').fullCalendar('refetchEvents');
   }
 });
